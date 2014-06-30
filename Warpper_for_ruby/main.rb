8b9440aa0c841ddd7e07703fb3e4cad52a0f8d68
Anton <anton_chikin_93@mail.ru>
2014-06-30 19:38:21 +0800

    Update main.rb

--- Warpper_for_ruby/main.rb
+++ Warpper_for_ruby/main.rb
@@ -7,6 +7,11 @@ x = Big_integer::Big_integer.new()
 y = Big_integer::Big_integer.new()
 z = Big_integer::Big_integer.new()
 
+if ARGV.length()==1 and ARGV[0]=="--help"
+	puts usage
+	exit 0
+end
+
 if ARGV.length() < 4
 	puts "Error: so few arguments"
 	exit 1
